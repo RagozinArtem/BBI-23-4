f216f4e54bef3bd3786b45e8628bbc5cf0cf9fae
RagozinArtem <m2308500@edu.misis.ru>
2024-03-30 16:52:15 +0300

    2

--- 7 лаб/2zad7thlab.cs
+++ 7 лаб/2zad7thlab.cs
@@ -6,11 +6,11 @@ class Program
 {
     class chelovek// структура
     {
-        private string _name;
-        private double[] _1ekz = new double[4];
-        private double _AVG;
+        protected string _name;
+        protected double[] _1ekz = new double[4];
+        protected double _AVG;
         public double AVG { get { return _AVG; } }// свойство
-        public tablica(string name, double[] ekz)
+        public chelovek(string name, double[] ekz)
         {
             _name = name;
             _1ekz = ekz;
@@ -25,35 +25,57 @@ class Program
                     _AVG = sum / 4;
                 }
             }
+
         }
-        public void Print()
+        public virtual void Print()
         {
             if (_AVG >= 4)
             {
-                Console.WriteLine("{0},   {1}", _name, _AVG);
+                Console.WriteLine("{0,20},{1,20}", _name, _AVG);
             }
         }
+
     }
     class student : chelovek
     {
+        protected int _studentnumber;
+
+        public student(string name, double[] ekz, int studentnumber) : base(name, ekz)
+        {
+            _studentnumber = studentnumber;
+        }
+        public override void Print()
+        {
+            if (_AVG >= 4)
+            {
+                Console.WriteLine("{0,20},{1,20},{2,20}", _name, _AVG, _studentnumber);
+            }
+        }
 
     }
     static void Main()
     {
-        tablica[] results = new tablica[5];
-        results[0] = new tablica("Sidorov", new double[] { 3, 4, 5, 2 });
-        results[1] = new tablica("Petrov", new double[] { 2, 3, 4, 4 });
-        results[2] = new tablica("Ivanov", new double[] { 5, 5, 4, 5 });
-        results[3] = new tablica("Kostin", new double[] { 4, 4, 3, 5 });
-        results[4] = new tablica("Smislov", new double[] { 4, 4, 4, 4 });
+        chelovek[] results = new chelovek[5];
+        results[0] = new chelovek("Sidorov", new double[] { 3, 4, 5, 2 });
+        results[1] = new chelovek("Petrov", new double[] { 2, 3, 4, 4 });
+        results[2] = new chelovek("Ivanov", new double[] { 5, 5, 4, 5 });
+        results[3] = new chelovek("Kostin", new double[] { 4, 4, 3, 5 });
+        results[4] = new chelovek("Smislov", new double[] { 4, 4, 4, 4 });
         sort(results);
+        student[] end = new student[5];
+        end[0] = new student("Sidorov", new double[] { 3, 4, 5, 2 }, 1);
+        end[1] = new student("Petrov", new double[] { 2, 3, 4, 4 }, 2);
+        end[2] = new student("Ivanov", new double[] { 5, 5, 4, 5 }, 3);
+        end[3] = new student("Kostin", new double[] { 4, 4, 3, 5 }, 4);
+        end[4] = new student("Smislov", new double[] { 4, 4, 4, 4 }, 5);
 
-        Console.WriteLine("Фамилия" + "   Средний бал");
+
+        Console.WriteLine("{0,20},{1,20},{2,20}", "Фамилия", "Средний бал", "Номер студентческого билета");
         for (int i = 0; i < results.Length; i++)
 
-        { results[i].Print(); }
+        { end[i].Print(); }
     }
-    static void sort(tablica[] results)
+    static void sort(chelovek[] results)
     {
         for (int i = 0; i < results.Length - 1; i++)
         {
@@ -62,7 +84,7 @@ class Program
             {
                 if (results[i].AVG < results[j].AVG)
                 {
-                    tablica all = results[j];
+                    chelovek all = results[j];
                     results[j] = results[i];
                     results[i] = all;
                 }
