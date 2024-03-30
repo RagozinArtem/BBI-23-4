4553a117f1eec3f6c522d0effee401b131f06c61
RagozinArtem <m2308500@edu.misis.ru>
2024-03-30 16:01:19 +0300

    первое

---
+++ первая/Program.cs
@@ -0,0 +1,72 @@
+﻿class sorevnovania// создаём структуру 
+{
+    private string _Surname; // поля структуры
+    private int _besttry;
+    private int _firsttry;
+    private int _secondtry;
+    private int _diskval;
+    public int Besttry { get { return _besttry; } } // свойство
+    public sorevnovania(int firsttry, int secondtry, string surname, int Diskval)// конструктор
+    {
+        _firsttry = firsttry;
+        _secondtry = secondtry;
+        _besttry = 0;
+        _Surname = surname;
+        _diskval = Diskval;
+        if (firsttry > secondtry)
+        {
+            _besttry = firsttry;
+        }
+        else if (secondtry >= firsttry) { _besttry = secondtry; }
+    }
+    public void Diskval(int diskval)
+    {
+        if (_diskval == 1)
+        {
+            _besttry = 0;
+        }
+    }
+    public void Print()
+    {
+        Diskval(_diskval);
+        if (_besttry > 0)
+        { Console.WriteLine($"{_besttry}, {_Surname}"); } // метод вывода
+    }
+}
+class Program
+{
+    static void Main()// мэйн
+    {
+        sorevnovania[] everything = new sorevnovania[5];// присваиваем тип sorevnovania для переменной everything 
+        everything[0] = new sorevnovania(155, 176, "Ivanov", 0);
+        everything[1] = new sorevnovania(180, 187, "Petrov", 1);
+        everything[2] = new sorevnovania(181, 191, "Sidorov", 0);
+        everything[3] = new sorevnovania(165, 186, "Ivanov", 0);
+        everything[4] = new sorevnovania(175, 171, "Ivanov", 0);
+        sort(everything);
+        for (int i = 0; i < everything.Length; i++)
+        {
+            everything[i].Print();
+        }
+    }
+    static void sort(sorevnovania[] everything)
+    {
+        int d = everything.Length / 2;
+        while (d >= 1)
+        {
+            for (int i = d; i < everything.Length; i++)
+            {
+                sorevnovania l = everything[i];
+                int k = everything[i].Besttry;
+                int j = i - d;
+                while (j >= 0 && everything[j].Besttry < k)
+                {
+                    everything[j + d] = everything[j];
+                    j -= d;
+                }
+                everything[j + d] = l;
+            }
+            d = d / 2;
+        }
+    }
+}
