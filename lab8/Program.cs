f98fffb5ea39910d9d3b369c8427a65b06901352
RagozinArtem <m2308500@edu.misis.ru>
2024-04-28 17:21:28 +0300

    2try

--- lab8/Program.cs
+++ lab8/Program.cs
@@ -1,5 +1,4 @@
-﻿
-//№ 1, 3, 6, 12, 13, 15
+﻿//№ 1, 3, 6, 12, 13, 15
 
 
 using System;
@@ -9,8 +8,11 @@ using System.Globalization;
 using System.Net.Security;
 using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Schema;
 using static System.Net.Mime.MediaTypeNames;
 
 abstract class Zadanie
@@ -25,10 +27,12 @@ abstract class Zadanie
 }
 class Thefirst : Zadanie
 {
-    private string _procentstr;
-    private double _procent;
+    private string _newtext;
+
+    private double[] _procent = new double[33];
     private double _countall;
-    private double _count;
+    private double[] _count = new double[33];
+    private char[] _pattern = new char[33] { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
     public Thefirst(string text) : base(text)
     {
 
@@ -36,17 +40,28 @@ class Thefirst : Zadanie
 
     public override string ToString()
     {
+        _newtext = _text.ToLower();
         for (int i = 0; i < _text.Length; i++)
         {
             if (Char.IsLetter(_text[i]))
             { _countall++; }
-            if (_text[i] == 'а')
-            { _count++; };
+            for (int j = 0; j < 33; j++)
+            {
+                if (_newtext[i] == _pattern[j])
+                { _count[j]++; };
+            }
         }
-        _procent = _count / _countall;
-        _procentstr = _procent.ToString("f4");
-        Console.WriteLine(_procentstr);
-        return _procentstr;
+        for (int i = 0; i < 33; i++)
+        {
+            _procent[i] = (_count[i] / _countall) * 100;
+        }
+
+        for (int i = 0; i < 33; i++)
+        {
+            Console.WriteLine(_pattern[i].ToString() + "= " + Math.Round(_procent[i], 2));
+
+        }
+        return base.ToString();
     }
 }
 class Thethird : Zadanie
@@ -77,88 +92,64 @@ class Thethird : Zadanie
 }
 class task6 : Zadanie
 {
-    private string _words;
-    private string _pattern;
-
+    private int[] _symbols = new int[10];
+    private int _symbolcount;
+    private string[] _words;
     public task6(string text) : base(text)
     {
-        _words = "";
-        _pattern = @"(?<=[аеиоуюяэыё])";
+        _words = _text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        _symbolcount = 0;
     }
+
+
     public override string ToString()
     {
-
-        for (int i = 0; i < _text.Length; i++)
+        foreach (string word in _words)
         {
-            _words = _words + _text[i];
+            _symbolcount = Countsymbols(word); // определяем количество слогов в слове           
+            _symbols[_symbolcount]++; // увеличиваем счетчик слов с определенным количеством слогов
+
         }
-        string[] syllables = Regex.Split(_words, _pattern);
-        int oneSyllable = 0;
-        int twoSyllables = 0;
-        int threeSyllables = 0;
-        int moreSyllables = 0;
-        foreach (string word in syllables)
+
+        for (int i = 1; i < _symbols.Length; i++) // выводим результат
         {
-            int syllableCount = CountSyllables(word);
-            if (syllableCount == 1)
-                oneSyllable++;
-            else if (syllableCount == 2)
-                twoSyllables++;
-            else if (syllableCount == 3)
-                threeSyllables++;
-            else
-                moreSyllables++;
-        }
-        static int CountSyllables(string word)
-        {
-            word = word.ToLower();
-            word = Regex.Replace(word, @"[^аеёиоуыэюя]", "");
-            return word.Length;
-        }
-        Console.WriteLine("Количество слов с одним слогом: " + oneSyllable);
-        Console.WriteLine("Количество слов с двумя слогами: " + twoSyllables);
-        Console.WriteLine("Количество слов с тремя слогами: " + threeSyllables);
-        Console.WriteLine("Количество слов с более чем тремя слогами: " + moreSyllables);
-        return oneSyllable.ToString("f3");
-
-
-
-    }
-}
-class Task12 : Zadanie
-{
-
-    public Task12(string text) : base(text)
-    {
-
-    }
-    public override string ToString()
-    {
-        Dictionary<string, string> wordCodes = new Dictionary<string, string>();
-        string[] lines = _text.Split(Environment.NewLine);
-        foreach (var line in lines)
-        {
-            if (line.Contains(":"))
+            if (_symbols[i] > 0)
             {
-                string[] parts = line.Split(":");
-                wordCodes[parts[1].Trim()] = parts[0].Trim();
+                Console.WriteLine($"Количество слов с {i} слогами: {_symbols[i]}");
+            }
+        }
+
+
+
+
+
+        return base.ToString();
+    }
+
+    private static int Countsymbols(string word)
+    {
+        int count = 0;
+        bool glasnie = false;
+
+        char[] glasnie_ = { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+
+        foreach (char letter in word)
+        {
+            if (Array.IndexOf(glasnie_, char.ToLower(letter)) >= 0)
+            {
+                if (!glasnie)
+                {
+                    glasnie = true;
+                    count++;
+                }
             }
             else
             {
-                _text += line + " ";
+                glasnie = false;
             }
         }
-        wordCodes["William"] = "#$@^";
-        wordCodes["one"] = "*@!&";
-        wordCodes["total"] = "!@#$%";
-        wordCodes["along"] = "%^&*(";
-        wordCodes["poems"] = "&*()!";
-        foreach (var code in wordCodes)
-        {
-            _text = _text.Replace(code.Key, code.Value);
-        }
-        Console.WriteLine(_text);
-        return _text.Trim();
+
+        return count;
     }
 }
 class task13 : Zadanie
@@ -195,7 +186,7 @@ class task13 : Zadanie
         for (int i = 0; i < 26; i++)
 
         {
-            Console.WriteLine(_meet[i]);
+            Console.WriteLine(_engl[i].ToString() + " = " + Math.Round(_meet[i], 2));
 
         }
 
@@ -205,53 +196,114 @@ class task13 : Zadanie
 }
 class task15 : Zadanie
 {
-    private string _chisla;
-    private double _count;
+
+    private string[] _words;
+    private int _sum;
 
     public task15(string text) : base(text)
     {
-        _count = 0;
-        _chisla = "";
-
+        _sum = 0;
+        _words = _text.Split(' ');
     }
 
     public override string ToString()
     {
-        for (int i = 0; i < _text.Length; i++)
 
+        foreach (string word in _words)
         {
-            if (Char.IsDigit(_text[i]))
+            // Преобразовать найденное число в int и добавить к сумме
+            if (int.TryParse(word, out int number))
             {
-                _chisla = _text[i].ToString();
-                _count += double.Parse(_chisla);
-
+                _sum += number;
             }
-
         }
-        Console.WriteLine(_count);
+        Console.WriteLine(_sum);
         return base.ToString();
+    }
+}
+class Task_12 : Zadanie
+{
+    private List<string> _wordsAndPunctuation = new List<string>();
+    private List<int> _wordCodes = new List<int>();
+
+
+    public Task_12(string text) : base(text) { }
+
+    public void ParseText(string text)
+    {
+        string[] wordsAndPunctuationArray = Regex.Split(text, @"(\b\S+\b|[^\w\s])");
+        int code = 1;
+
+        foreach (string item in wordsAndPunctuationArray)
+        {
+            if (Regex.IsMatch(item, @"\b\S+\b"))
+            {
+                _wordsAndPunctuation.Add(item);
+                _wordCodes.Add(code);
+                code++;
+            }
+            else
+            {
+                _wordsAndPunctuation.Add(item);
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        string result = "Decoded Text:\n";
+        foreach (string symb in _wordsAndPunctuation)
+        {
+            if (symb.StartsWith("#"))
+            {
+                int code = int.Parse(symb.Substring(1));
+                string word = GetDecodedWord(code);
+                result += word + " ";
+            }
+            else
+            {
+                result += symb;
+            }
+        }
+        Console.WriteLine(result);
+        return result;
+    }
+
+    private string GetDecodedWord(int code)
+    {
+        int index = code - 1;
+        if (index >= 0 && index < _wordsAndPunctuation.Count)
+        {
+            return _wordsAndPunctuation[index];
+        }
+        return "";
     }
 }
 
 
+
+
+
 class Program
 {
-    static void Main()
+    public static void Main()
     {
         Thefirst first = new Thefirst("После многолетних исследований ученые обнаружили тревожную тенденцию в вырубке лесов Амазонии. Анализ данных показал, что основной участник разрушения лесного покрова – человеческая деятельность. За последние десятилетия рост объема вырубки достиг критических показателей. Главными факторами, способствующими этому, являются промышленные рубки, производство древесины, расширение сельскохозяйственных угодий и незаконная добыча древесины. Это приводит к серьезным экологическим последствиям, таким как потеря биоразнообразия, ухудшение климата и угроза вымирания многих видов животных и растений. ");
         Thethird third = new Thethird("Двигатель самолета – это сложная инженерная конструкция, обеспечивающая подъем, управление и движение в воздухе. Он состоит из множества компонентов, каждый из которых играет важную роль в общей работе механизма. Внутреннее устройство двигателя включает в себя компрессор, камеру сгорания, турбину и системы управления и охлаждения. Принцип работы основан на воздушно-топливной смеси, которая подвергается сжатию, воспламенению и расширению, обеспечивая движение воздушного судна.");
         task6 sixth = new task6("1 июля 2015 года Греция объявила о дефолте по государственному долгу, став первой развитой страной в истории, которая не смогла выплатить свои долговые обязательства в полном объеме. Сумма дефолта составила порядка 1,6 миллиарда евро. Этому предшествовали долгие переговоры с международными кредиторами, такими как Международный валютный фонд (МВФ), Европейский центральный банк (ЕЦБ) и Европейская комиссия (ЕК), о программах финансовой помощи и реструктуризации долга. Основными причинами дефолта стали недостаточная эффективность реформ, направленных на улучшение финансовой стабильности страны, а также политическая нестабильность, что вызвало потерю доверия со стороны международных инвесторов и кредиторов. Последствия дефолта оказались глубокими и долгосрочными: сокращение кредитного рейтинга страны, увеличение затрат на заемный капитал, рост стоимости заимствований и утрата доверия со стороны международных инвесторов.");
-        Task12 twelve = new Task12("Фьорды – это ущелья, формирующиеся ледниками и заполняющиеся морской водой. Название происходит от древнескандинавского слова \"fjǫrðr\". Эти глубокие заливы, окруженные высокими горами, представляют захватывающие виды и природную красоту. Они популярны среди туристов и известны под разными названиями: в Норвегии – \"фьорды\", в Шотландии – \"фьордс\", в Исландии – \"фьордар\". Фьорды играют важную роль в культуре и туризме региона, продолжая вдохновлять людей со всего мира.");
         task13 thirtinth = new task13("William Shakespeare, widely regarded as one of the greatest writers in the English language, authored a total of 37 plays, along with numerous poems and sonnets. He was born in Stratford-upon-Avon, England, in 1564, and died in 1616. Shakespeare's most famous works, including \"Romeo and Juliet,\" \"Hamlet,\" \"Macbeth,\" and \"Othello,\" were written during the late 16th and early 17th centuries. \"Romeo and Juliet,\" a tragic tale of young love, was penned around 1595. \"Hamlet,\" one of his most celebrated tragedies, was written in the early 1600s, followed by \"Macbeth,\" a gripping drama exploring themes of ambition and power, around 1606. \"Othello,\" a tragedy revolving around jealousy and deceit, was also composed during this period, believed to be around 1603.");
+        Task_12 task12 = new Task_12("После многолетних исследований ученые обнаружили тревожную тенденцию в вырубке лесов Амазонии. Анализ данных показал, что основной участник разрушения лесного покрова – человеческая деятельность. За последние десятилетия рост объема вырубки достиг критических показателей. Главными факторами, способствующими этому, являются промышленные рубки, производство древесины, расширение сельскохозяйственных угодий и незаконная добыча древесины. Это приводит к серьезным экологическим последствиям, таким как потеря биоразнообразия, ухудшение климата и угроза вымирания многих видов животных и растений. ");
+        task15 fifteen = new task15("Первое кругосветное путешествие было осуществлено флотом, возглавляемым португальским исследователем Фернаном Магелланом. Путешествие началось 20 сентября 1519 года, когда флот, состоящий из пяти кораблей и примерно 270 человек, отправился из порту Сан-Лукас в Испании. Хотя Магеллан не закончил свое путешествие из-за гибели в битве на Филиппинах в 1521 году, его экспедиция стала первой, которая успешно обогнула Землю и доказала ее круглую форму. Это путешествие открыло новые морские пути и имело огромное значение для картографии и географических открытий.");
 
         sixth.ToString();
         first.ToString();
         third.ToString();
         thirtinth.ToString();
-        task15 fifteen = new task15("Первое кругосветное путешествие было осуществлено флотом, возглавляемым португальским исследователем Фернаном Магелланом. Путешествие началось 20 сентября 1519 года, когда флот, состоящий из пяти кораблей и примерно 270 человек, отправился из порту Сан-Лукас в Испании. Хотя Магеллан не закончил свое путешествие из-за гибели в битве на Филиппинах в 1521 году, его экспедиция стала первой, которая успешно обогнула Землю и доказала ее круглую форму. Это путешествие открыло новые морские пути и имело огромное значение для картографии и географических открытий.");
         fifteen.ToString();
-        twelve.ToString();
+        task12.ParseText("После многолетних исследований ученые обнаружили тревожную тенденцию в вырубке лесов Амазонии. Анализ данных показал, что основной участник разрушения лесного покрова – человеческая деятельность. За последние десятилетия рост объема вырубки достиг критических показателей. Главными факторами, способствующими этому, являются промышленные рубки, производство древесины, расширение сельскохозяйственных угодий и незаконная добыча древесины. Это приводит к серьезным экологическим последствиям, таким как потеря биоразнообразия, ухудшение климата и угроза вымирания многих видов животных и растений. ");
+        task12.ToString();
     }
-
 }
+
+
 
